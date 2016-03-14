b93d352a995b6cc82fb97f2e538c1f64303c8395
arthurgca <arthur.azevedo@ccc.ufcg.edu.br>
2016-03-14 18:35:12 -0300

    Layout inicial

---
+++ app/src/main/java/projetoes/com/floppyalarm/RowAdapter.java
@@ -0,0 +1,53 @@
+package projetoes.com.floppyalarm;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class RowAdapter extends BaseAdapter implements ListAdapter {
+    private ArrayList<String> list = new ArrayList<String>();
+    private Context context;
+
+    public RowAdapter(ArrayList<String> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int pos) {
+        return list.get(pos);
+    }
+
+    @Override
+    public long getItemId(int pos) {
+        return 0;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.content_main_row, null);
+        }
+        TextView listItemText = (TextView) view.findViewById(R.id.textView);
+        listItemText.setText(list.get(position));
+
+        Button switchBtn = (Button) view.findViewById(R.id.switch1);
+
+        return view;
+    }
+}
+
